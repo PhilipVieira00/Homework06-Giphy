48fa005b17a12095bd983310a50c2a1c974893cf
Philip Vieira <philipvieira00@gmail.com>
2019-06-25 22:32:05 -0400

    Cannibalized various parts of preexisting code to create preliminary layout

---
+++ function.js
@@ -0,0 +1,9 @@
+
+if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  }
+  else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
